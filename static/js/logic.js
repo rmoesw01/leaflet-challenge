858b5f927e6b2661a717085433d31acb2772a570
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-09-15 20:43:16 -0400

    added earthquake layer

--- static/js/logic.js
+++ static/js/logic.js
@@ -2,17 +2,57 @@
 url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
 function displayMap(inputData) {
-
+    
     // Puts the place and time in a popup for the feature
     function onEachFeature(feature, layer) {
+
         layer.bindPopup("<h3>" + feature.properties.place +
           "</h3><hr><p>" + new Date(feature.properties.time) + 
           "</p><br><p>Magnitude:" + feature.properties.mag + "</p>");
     }
 
+    function createCircles(feature){
+        magnitude = feature.properties.mag
+        // for (var i = 0; i < feature.length; i++) {
+        var color = "";
+
+        if (magnitude > 5) {
+            color = '#FF0000';
+        }
+        else if (magnitude > 4) {
+            color = '#FF6600';
+        }
+        else if (magnitude > 3) {
+            color = '#FF9900';
+        }
+        else if (magnitude > 2) {
+            color = '#FFCC00';
+        }
+        else if (magnitude > 1) {
+            color = '#FFFF00';
+        }
+        else {
+            color = '#00FF00';
+        }
+        // }
+
+        var geojsonMarkerOptions = {
+            radius: magnitude * 5,
+            fillColor: color,
+            color: color,
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        };
+        var latlng = L.latLng([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
+        console.log(latlng);
+        return L.circleMarker(latlng, geojsonMarkerOptions);
+    }
+
     // create GeoJSON layer
     var earthquakes = L.geoJSON(inputData, {
-        onEachFeature: onEachFeature
+        onEachFeature: onEachFeature,
+        pointToLayer: createCircles
     });
 
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
@@ -24,9 +64,9 @@ function displayMap(inputData) {
 
     var myMap = L.map("map", {
         center: [
-          37.09, -95.71
+          0, 0
         ],
-        zoom: 5,
+        zoom: 2,
         layers: [lightmap, earthquakes]
       });
 }
