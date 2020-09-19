ef0befc89a022f439a786dee01925d3b8396bd49
Becki Oeswein Leeds <rmoesw01@gmail.com>
2020-09-19 19:08:55 -0400

    Comment bonus.js script file'

--- static/js/bonus.js
+++ static/js/bonus.js
@@ -6,12 +6,13 @@ function displayMap(inputData, inputData2) {
     console.log(inputData);
     // Puts the place and time in a popup for the feature
     function onEachFeature(feature, layer) {
-
+        // add a popup to each marker
         layer.bindPopup("<h3>" + feature.properties.place +
           "</h3><hr><p>" + new Date(feature.properties.time) + 
           "</p><br><p>Magnitude:" + feature.properties.mag + "</p>");
     }
 
+    // function to get the color of the marker based on the magnitude of the earthquake
     function getColor(magnitude) {
         var color = "";
 
@@ -36,11 +37,12 @@ function displayMap(inputData, inputData2) {
         return color;
     }
 
+    // function to create the circular markers with size and color based on the earthquake magnitude
     function createCircles(feature){
         magnitude = feature.properties.mag;
         var color = getColor(magnitude);
-        // }
 
+        // set the style settings for the markers
         var geojsonMarkerOptions = {
             radius: magnitude * 5,
             fillColor: color,
@@ -49,43 +51,51 @@ function displayMap(inputData, inputData2) {
             opacity: 1,
             fillOpacity: 0.8
         };
+
+        // get the latitude and longitude of the earthquake
         var latlng = L.latLng([feature.geometry.coordinates[1],feature.geometry.coordinates[0]])
-        // console.log(latlng);
+        
+        // return the created circular marker
         return L.circleMarker(latlng, geojsonMarkerOptions);
     }
 
+    // create layer groups for the overlay layers
     var earthquakes = L.layerGroup();
     var faultLines = L.layerGroup();
 
-    // create GeoJSON layer
+    // create earthquake GeoJSON layer
     L.geoJSON(inputData, {
         onEachFeature: onEachFeature,
         pointToLayer: createCircles
     }).addTo(earthquakes);
 
+    // create faultline GeoJSON layer
      L.geoJSON(inputData2, {
          fillOpacity: 0
-     }).addTo(faultLines);
+    }).addTo(faultLines);
 
+    // create the Base Layer with a light background
     var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "light-v10",
-    accessToken: API_KEY
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "light-v10",
+        accessToken: API_KEY
     });
 
+    // create the Base Layer with an outdoor background
     var outdoors = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "outdoors-v9",
-    accessToken: API_KEY
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "outdoors-v9",
+        accessToken: API_KEY
     });
 
+    // create the Base Layer with a satellite background
     var satellite = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-    attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-    maxZoom: 18,
-    id: "satellite-streets-v9",
-    accessToken: API_KEY
+        attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+        maxZoom: 18,
+        id: "satellite-streets-v9",
+        accessToken: API_KEY
     });
 
     // Create a baseMaps object
@@ -101,6 +111,7 @@ function displayMap(inputData, inputData2) {
         ,"Fault Lines": faultLines
     };
 
+    // Create the map with the default settings
     var myMap = L.map("map", {
         center: [
           0, 0
@@ -109,6 +120,7 @@ function displayMap(inputData, inputData2) {
         layers: [lightmap, earthquakes]
     });
 
+    // add the control to the map where the user can choose the layers they wish to display
     L.control.layers(baseMaps, overlayMaps, {
         collapsed: false
     }).addTo(myMap);
@@ -116,6 +128,7 @@ function displayMap(inputData, inputData2) {
     /*Legend specific*/
     var legend = L.control({ position: "bottomright" });
 
+    // create the color and value display of the legend
     legend.onAdd = function(myMap) {
         var div = L.DomUtil.create("div", "legend");
         div.innerHTML += '<i style="background: #00FF00"></i><span>0-1</span><br>';
@@ -131,8 +144,11 @@ function displayMap(inputData, inputData2) {
       legend.addTo(myMap);
 }
 
+// read in the earthquake data
 d3.json(url, function(response) {
+    // read in the faultline data
     d3.json('static/data/plates.json', function(plateData){
+        // call the displayMap function, passing the two data sets
         displayMap(response.features, plateData.features);
     });
 });
